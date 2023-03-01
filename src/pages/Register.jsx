a6b3af499666513fbbcb61a025029a3e37dc6bbf
bluuweb <contacto@bluuweb.cl>
2023-03-01 10:17:56 -0400

    text Field

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,61 @@
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+
 export default function Register() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState({
+    error: false,
+    message: "",
+  });
+
+  const validateEmail = (email) => {
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return regex.test(email);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateEmail(email)) {
+      setError({
+        error: false,
+        message: "",
+      });
+      console.log("Email correcto");
+    } else {
+      setError({
+        error: true,
+        message: "Formato de email incorrecto",
+      });
+    }
+  };
+
   return (
     <>
       <h1>Register</h1>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          id="email"
+          label="Email"
+          type="email"
+          variant="outlined"
+          fullWidth
+          required
+          error={error.error}
+          helperText={error.message}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Button
+          type="submit"
+          variant="outlined"
+          sx={{ mt: 2 }}
+        >
+          Registrame
+        </Button>
+      </Box>
     </>
   );
 }
